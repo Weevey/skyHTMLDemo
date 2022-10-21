5fd420298bac4037fcd22ab426d34443deedd63c
Steve Ayres <weevey@hotmail.co.uk>
2022-10-21 16:29:13 +0100

    Adding all files to repo

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+'use strict'
+
+function myFunction() {
+    document.getElementById('change').src='/IMAGES/sky.jpeg';
+    } 
+function revert() {
+    document.getElementById('change').src='/IMAGES/mrew.png';
+    }
+
+function buttpress() {
+    document.getElementById('name').innerHTML = 'Ayres';
+}
